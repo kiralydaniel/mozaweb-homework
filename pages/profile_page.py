da77530a71cc13fbf842659c212b2f5fb1352505
kiralydaniel <kiralydani93@gmail.com>
2025-03-27 13:31:04 +0100

    add order history checks to ProfilePage

--- pages/profile_page.py
+++ pages/profile_page.py
@@ -8,6 +8,11 @@ class ProfilePage(BasePage):
         #Locators
         self.profile_menu = page.get_by_role("link", name="Welcome back")
 
-
     def user_is_logged_in(self):
         expect(self.profile_menu).to_be_visible()
+
+    def navigate_to_order_history(self):
+        self.navigate("/index.php?rt=account/history")
+
+    def verify_order_id(self, order_number):
+        expect(self.page.get_by_text(f"Order ID: #{order_number}")).to_be_visible()
