0c60538f684ed7a415b10448b0d71f086c71fbf1
kiralydaniel <kiralydani93@gmail.com>
2025-03-27 10:10:56 +0100

    update purchase flow test

--- tests/test_purchase_flow.py
+++ tests/test_purchase_flow.py
@@ -1,5 +1,6 @@
 from playwright.sync_api import Page
 from pages.registration_page import RegistrationPage
+from pages.shopping_page import ShoppingPage
 from utils.test_data import generate_test_user
 
 
@@ -7,10 +8,16 @@ from utils.test_data import generate_test_user
 def test_purchase_flow(page: Page):
     # Initialize page objects
     registration_page = RegistrationPage(page)
+    shopping_page = ShoppingPage(page)
 
     # Register new user
     user_data = generate_test_user()
     registration_page.navigate_to_register()
     registration_page.fill_registration_form(user_data)
     registration_page.submit_registration()
-    
+
+    # Navigate to shirts and add the two most expensive to cart
+    shopping_page.navigate_to_shirts()
+    shopping_page.sort_by_price_desc()
+    shopping_page.add_most_expensive_to_cart()
+    shopping_page.go_to_checkout()
