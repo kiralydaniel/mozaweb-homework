5ed963a4241388085be95cf56579d8f7b6f1ab4a
kiralydaniel <kiralydani93@gmail.com>
2025-03-27 12:31:15 +0100

    implement order confirmation flow

--- tests/test_purchase_flow.py
+++ tests/test_purchase_flow.py
@@ -2,6 +2,7 @@ from playwright.sync_api import Page
 from pages.registration_page import RegistrationPage
 from pages.shopping_page import ShoppingPage
 from pages.profile_page import ProfilePage
+from pages.checkout_page import CheckoutPage
 from utils.test_data import generate_test_user
 
 
@@ -11,6 +12,7 @@ def test_purchase_flow(page: Page):
     registration_page = RegistrationPage(page)
     shopping_page = ShoppingPage(page)
     profile_page = ProfilePage(page)
+    checkout_page = CheckoutPage(page)
 
     # Register new user
     user_data = generate_test_user()
@@ -26,3 +28,9 @@ def test_purchase_flow(page: Page):
     shopping_page.sort_by_price_desc()
     shopping_page.add_most_expensive_to_cart()
     shopping_page.go_to_checkout()
+
+    checkout_page.confirm_order()
+    checkout_page.is_order_successful()
+    order_number = checkout_page.extract_order_number()
+
+
